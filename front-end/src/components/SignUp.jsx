b67ef840ea01367169427aabfed97bb3da696987
Melannie Castro <melanniecastrovargas2002@gmail.com>
2022-06-13 14:02:10 -0500

    Sign up desing and layout

--- front-end/src/components/SignUp.jsx
+++ front-end/src/components/SignUp.jsx
@@ -1,10 +1,24 @@
-
+import '../index.css'
 
 export function SignUp() {
 
   return(
     <>
-      
+      <div className="container">
+        <div className="container_sign-up">
+          <div className="container_form">
+            <h1>Sign Up</h1>
+            <form className="form">
+              <input type="text" placeholder="Name" className="form_input"/>
+              <input type="text" placeholder="Last Name" className="form_input"/>
+              <input type="email" placeholder="Email" className="form_input"/>
+              <input type="password" placeholder="Password" className="form_input"/>
+              <input type="submit" value="CREATE ACCOUNT" className="form_button"/>
+            </form>
+              <a className="form_link-signin" href="/">Sign in</a>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
