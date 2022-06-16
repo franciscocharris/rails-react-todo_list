b0e8ec93ec67226fd4724f73bd0695d6a896abf7
Francisco Charris <francisco.charris@koombea.com>
2022-06-16 16:07:34 -0500

    user_id to tasks migration runned

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_16_192510) do
+ActiveRecord::Schema.define(version: 2022_06_16_210412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,9 @@ ActiveRecord::Schema.define(version: 2022_06_16_192510) do
     t.bigint "list_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["list_id"], name: "index_tasks_on_list_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,4 +46,5 @@ ActiveRecord::Schema.define(version: 2022_06_16_192510) do
 
   add_foreign_key "lists", "users"
   add_foreign_key "tasks", "lists"
+  add_foreign_key "tasks", "users"
 end
